425b06dc8485c3a9be53075030a4329b7700ad5b
George Lemon <georgelemon@protonmail.com>
2023-05-30 23:43:37 +0300

    store `Message` instead of string
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/bropkg/engine/logging.nim
+++ src/bropkg/engine/logging.nim
@@ -55,7 +55,8 @@ type
     lvlError
 
   Log* = ref object
-    msg, extraLabel: string
+    msg: Message
+    extraLabel: string
     line, col: int
     useFmt: bool
     args, extraLines: seq[string]
@@ -66,7 +67,7 @@ type
 
 proc add(logger: Logger, lvl: Level, msg: Message, line, col: int,
         useFmt: bool, args: varargs[string]) =
-  let log = Log(msg: $msg, args: args.toSeq(),
+  let log = Log(msg: msg, args: args.toSeq(),
                 line: line, col: col + 1, useFmt: useFmt)
   case lvl:
     of lvlInfo:
@@ -81,7 +82,7 @@ proc add(logger: Logger, lvl: Level, msg: Message, line, col: int,
 proc add(logger: Logger, lvl: Level, msg: Message, line, col: int, useFmt: bool,
         extraLines: seq[string], extraLabel: string, args: varargs[string]) =
   let log = Log(
-    msg: $msg,
+    msg: msg,
     args: args.toSeq(),
     line: line,
     col: col + 1,
@@ -98,6 +99,9 @@ proc add(logger: Logger, lvl: Level, msg: Message, line, col: int, useFmt: bool,
       logger.warnLogs.add(log)
     of lvlError:
       logger.errorLogs.add(log)
+
+proc getMessage*(log: Log): Message = 
+  result = log.msg
 
 proc newInfo*(logger: Logger, msg: Message, line, col: int,
               useFmt: bool, args:varargs[string]) =
@@ -158,15 +162,15 @@ proc runIterator(i: Log, label: string, fgColor: ForegroundColor): Row =
   add result, span("(" & $i.line & ":" & $i.col & ")")
   if i.useFmt:
     var x: int
-    var str = i.msg.split("$")
-    let length = i.msg.count("$") - 1
+    var str = split($i.msg, "$")
+    let length = count($i.msg, "$") - 1
     for s in str:
       add result, span(s.strip())
       if length >= x:
         add result, span(i.args[x], fgBlue)
       inc x
   else:
-    add result, span(i.msg)
+    add result, span($i.msg)
     for a in i.args:
       add result, span(a, fgBlue)
 
