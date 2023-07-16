503056c280f7b504e20fa3f8c18e58a69f55a1b3
George Lemon <georgelemon@protonmail.com>
2023-07-16 13:48:12 +0300

    wip dummy stdlib
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ src/bropkg/engine/stdlib.nim
@@ -0,0 +1,47 @@
+import std/[tables, macrocache, options, strutils]
+
+{.pragma: nim, cdecl, noSideEffect, gcsafe, locks:0.}
+
+type
+  Function* = object
+    id: int
+    value: pointer
+  StandardLibrary* = Table[string, TableRef[string, Function]]
+
+const counter = CacheCounter("counter")
+
+func typeId(tp: type): int =
+  const id = counter.value
+  static:
+    inc counter
+  id
+
+proc fn*[T](p: T): Function =
+  Function(id: T.typeId, value: cast[pointer](p))
+
+var strtable = newTable[string, Function]()
+var stdlib = newTable[string, TableRef[string, Function]]()
+
+proc call*(fnName: string, fn: type): Option[fn] =
+  let f = strtable[fnName]
+  if fn.typeId != f.id:
+    none(fn)
+  else:
+    some(cast[fn](f.value))
+
+type strCountFn = proc (str: string, sub: string): bool {.nim.}
+strtable["count"] = fn:
+  proc (str: string, sub: char): int {.nim.} =
+    str.count(sub)
+
+type strContainsFn = proc (str: string, sub: string): bool {.nim.}
+strtable["contains"] = fn:
+  proc (str: string, sub: string): bool {.nim.} =
+    str.contains(sub)
+
+type strFindFn = proc (str, sub: string, start = 0, last = - 1): int {.nim.}
+strtable["find"] = fn:
+  proc (str, sub: string, start = 0, last = - 1): int {.nim.} =
+    str.find(sub, start, last)
+
+stdlib["strings"] = strtable
