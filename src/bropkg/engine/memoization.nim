1a8fb7003c32df4c645b58e44c9b2d7afd2e0a47
George Lemon <georgelemon@protonmail.com>
2023-07-23 18:23:25 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/bropkg/engine/memoization.nim
+++ src/bropkg/engine/memoization.nim
@@ -6,19 +6,31 @@
 #          Made by Humans from OpenPeeps
 #          https://github.com/openpeeps/bro
 
-import std/[critbits, hashes]
+import std/[critbits, hashes, jsonutils, json]
 from ./ast import Node
+
+export hashes
 
 type
   MCall* = CritBitTree[Node] # ntCall
   MVar* = CritBitTree[Node] # ntVariable
 
-proc isMemoized*(memo: CritBitTree, ident: string): bool =
-  result = memo.contains($hash(ident))
+proc hashed*(nstr: string): Hash = nstr.hash
 
-proc getMemoized*(memo: CritBitTree, ident: string): Node =
-  try: memo[$hash(ident)]
+proc isMemoized*(memo: CritBitTree, ident: Hash): bool =
+  ## Checks if `ident` Hash is available in `memo` tree 
+  result = memo.contains($(ident))
+
+proc memoized*(memo: CritBitTree, ident: Hash): Node =
+  ## Get a memoized node from `memo` tree. Returns `nil` when not found.
+  try: memo[$(ident)]
   except KeyError: nil
 
-proc memoize*(memo: var CritBitTree, ident: string, node: Node) =
-  memo[$hash(ident)] = node
+proc memoize*(memo: var CritBitTree, ident: Hash, node: Node) =
+  ## Store a new `node` using hashed `ident`
+  memo[$(ident)] = node
+
+proc delete*(memo: CritBitTree, ident: Hash) =
+  ## Delete hashed `ident` from `memo`.
+  ## If the `ident` does not exists, nothing happens.
+  memo.excl($(ident))
