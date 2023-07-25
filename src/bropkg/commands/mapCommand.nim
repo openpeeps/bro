11a80b7996d2ee1e22649717386d9bec0b5c2e19
George Lemon <georgelemon@protonmail.com>
2023-07-25 23:22:31 +0300

    update license
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/bropkg/commands/mapCommand.nim
+++ src/bropkg/commands/mapCommand.nim
@@ -1,6 +1,6 @@
 # A super fast stylesheet language for cool kids
 #
-# (c) 2023 George Lemon | MIT License
+# (c) 2023 George Lemon | LGPL License
 #          Made by Humans from OpenPeeps
 #          https://github.com/openpeeps/bro
 
