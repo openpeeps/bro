2e5887c66e48bf7e04a92543c51f3ca648604768
George Lemon <georgelemon@protonmail.com>
2023-08-04 06:47:26 +0300

    update test
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -7,53 +7,53 @@ proc getPath(append: string): string =
 var paths = ["basic", "invalid", "for", "forjson", "case", "unused"]
 
 test "can parse":
-  var p = parseProgram(paths[0].getPath())
+  var p = parseStylesheet(paths[0].getPath())
   check p.hasErrors == false
   check p.hasWarnings == false
 
 test "can compile":
   let basic = paths[0].getPath()
-  var p = parseProgram(basic)
+  var p = parseStylesheet(basic)
   check p.hasErrors == false
   check p.hasWarnings == false
 
   var c: Compiler
   # unminified
-  c = newCompiler(p.getProgram)
+  c = newCompiler(p.getStylesheet)
   check c.getCSS.count("\n") == 3
   # minified
-  c = newCompiler(p.getProgram, minify = true)
+  c = newCompiler(p.getStylesheet, minify = true)
   check c.getCSS.count("\n") == 0
   check c.getCSS == ".btn{border:2px #FFF solid}"
 
 test "can catch errors":
-  var p = parseProgram(paths[1].getPath)
+  var p = parseStylesheet(paths[1].getPath)
   check p.hasErrors == true
   check p.logger.errorLogs[0].getMessage == invalidProperty
 
 test "can catch warnings":
-  var p = parseProgram(paths[5].getPath)
+  var p = parseStylesheet(paths[5].getPath)
   check p.hasErrors == false
   check p.hasWarnings == true
 
 test "can compile `for` blocks":
   let path = paths[2].getPath()
-  var p = parseProgram(path)
+  var p = parseStylesheet(path)
   check p.hasErrors == false
   check p.hasWarnings == false
-  let c = newCompiler(p.getProgram)
+  let c = newCompiler(p.getStylesheet)
 
 test "can compile `for` blocks w/ @json":
   let path = paths[3].getPath()
-  var p = parseProgram(path)
+  var p = parseStylesheet(path)
   check p.hasErrors == false
   check p.hasWarnings == false
-  let c = newCompiler(p.getProgram)
+  let c = newCompiler(p.getStylesheet)
 
 test "can compile `case` blocks":
   let path = paths[4].getPath()
-  var p = parseProgram(path)
+  var p = parseStylesheet(path)
   check p.hasErrors == false
   check p.hasWarnings == false
-  let c = newCompiler(p.getProgram)
+  let c = newCompiler(p.getStylesheet)
   echo c.getCSS
