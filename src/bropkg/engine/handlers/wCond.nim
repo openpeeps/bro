3bf91b682bfa7cc55e5d97d2d500e1660e2487d0
George Lemon <georgelemon@protonmail.com>
2023-08-07 03:07:47 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/bropkg/engine/handlers/wCond.nim
+++ src/bropkg/engine/handlers/wCond.nim
@@ -7,6 +7,7 @@
 proc handleCondStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
   # Compiler handler to evaluate conditional statements `if`, `elif`, `else`
   var
+    ix = 1
     tryElse: bool
     lht: Node = node.ifInfix.infixleft
     rht: Node = node.ifInfix.infixRight
@@ -21,8 +22,10 @@ proc handleCondStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
   if c.evalInfix(lht, node.ifInfix.infixRight,
             node.ifInfix.infixOp, scope):
     for ifNode in node.ifStmt.stmtList:
-      c.handleInnerNode(ifNode, parent, scope, node.ifStmt.stmtList.len)
+      c.handleInnerNode(ifNode, parent, scope, node.ifStmt.stmtList.len, ix)
     return # condition is truthy
+  ix = 1
+  # handle elfi branches
   if node.elifStmt.len > 0:
     for elifBranch in node.elifStmt: # walk through seq[Node] of `elif` statements
       case elifBranch.comp.nt
@@ -30,23 +33,28 @@ proc handleCondStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
         if c.evalInfix(elifBranch.comp.infixLeft, elifBranch.comp.infixRight,
                     elifBranch.comp.infixOp, scope):
           for elifNode in elifBranch.body.stmtList:
-            c.handleInnerNode(elifNode, parent, scope, elifBranch.body.stmtList.len)
+            c.handleInnerNode(elifNode, parent, scope, elifBranch.body.stmtList.len, ix)
           return # condition is truthy
       else:
         discard # handle variables or boolean literals
+  ix = 1
+  # handle else branch
   if node.elseStmt != nil:
+    let len = node.elseStmt.stmtList.len
     for elseNode in node.elseStmt.stmtList:
-      c.handleInnerNode(elseNode, parent, scope, node.elseStmt.stmtList.len)
+      c.handleInnerNode(elseNode, parent, scope, len, ix)
 
 proc handleCaseStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
   # Compiler handler to evaluate `case` `of` statements
+  var ix = 1
   for caseNode in node.caseCond:
     if c.evalInfix(node.caseIdent, caseNode.caseOf, EQ, scope):
       let len = caseNode.body.stmtList.len
       for caseBody in caseNode.body.stmtList:
-        c.handleInnerNode(caseBody, parent, scope, len)
+        c.handleInnerNode(caseBody, parent, scope, len, ix)
       return
+    ix = 1
   if node.caseElse != nil:
     let len = node.caseElse.stmtList.len
     for caseBody in node.caseElse.stmtList:
-      c.handleInnerNode(caseBody, parent, scope, len)
+      c.handleInnerNode(caseBody, parent, scope, len, ix)
--- src/bropkg/engine/handlers/wFor.nim
+++ src/bropkg/engine/handlers/wFor.nim
@@ -6,14 +6,16 @@
 
 proc handleForStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
   # Handle `for` statements
-  var itemsNode = 
-    if node.inItems.nt in {ntArray, ntObject}:
-      node.inItems
-    elif node.inItems.callNode != nil:
-      node.inItems.callNode
-    else:
-      assert scope != nil
-      scope[node.inItems.callIdent]
+  var
+    ix = 1
+    itemsNode = 
+      if node.inItems.nt in {ntArray, ntObject}:
+        node.inItems
+      elif node.inItems.callNode != nil:
+        node.inItems.callNode
+      else:
+        assert scope != nil
+        scope[node.inItems.callIdent]
   node.forStorage = ScopeTable()
   node.forStorage[node.forItem.varName] = node.forItem
   case itemsNode.nt:
@@ -21,24 +23,24 @@ proc handleForStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
     # Array or Object iterator via Variable call
     let items = itemsNode.varValue.arrayItems
     let len = node.forBody.stmtList.len
-    for i in 0 .. items.high:
-      node.forStorage[node.forItem.varName].varValue = items[i]
-      for ii in 0 .. node.forBody.stmtList.high:
-        c.handleInnerNode(node.forBody.stmtList[ii], parent, node.forStorage, len)
+    for item in 0 .. items.high:
+      node.forStorage[node.forItem.varName].varValue = items[item]
+      for iitem in 0 .. node.forBody.stmtList.high:
+        c.handleInnerNode(node.forBody.stmtList[iitem], parent, node.forStorage, len, ix)
   of ntArray:
     # Array iterator
     let len = node.forBody.stmtList.len
     for i in 0 .. itemsNode.arrayItems.high:
       node.forStorage[node.forItem.varName].varValue = itemsNode.arrayItems[i]
       for n in node.forBody.stmtList:
-        c.handleInnerNode(n, parent, node.forStorage, len)
+        c.handleInnerNode(n, parent, node.forStorage, len, ix)
   of ntStream:
     # Write JSON/YAML streams
     let len = node.forBody.stmtList.len
     for item in items(itemsNode.streamContent):
       node.forStorage[node.forItem.varName].varValue = newStream item
       for n in node.forBody.stmtList:
-        c.handleInnerNode(n, parent, node.forStorage, len)
+        c.handleInnerNode(n, parent, node.forStorage, len, ix)
   of ntAccessor:
     var x: Node 
     if itemsNode.accessorType == ntArray:
@@ -47,7 +49,7 @@ proc handleForStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
       for i in 0 .. x.arrayItems.high:
         node.forStorage[node.forItem.varName].varValue = x.arrayItems[i]
         for nodeBody in node.forBody.stmtList:
-          c.handleInnerNode(nodeBody, parent, node.forStorage, len)
+          c.handleInnerNode(nodeBody, parent, node.forStorage, len, ix)
       return
     x = walkAccessorStorage(itemsNode.accessorStorage, itemsNode.accessorKey, scope)
     if x.nt == ntObject:
@@ -55,7 +57,7 @@ proc handleForStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
       for i, y in x.pairsVal:
         node.forStorage[node.forItem.varName].varValue = y
         for nodeBody in node.forBody.stmtList:
-          c.handleInnerNode(nodeBody, parent, node.forStorage, len)
+          c.handleInnerNode(nodeBody, parent, node.forStorage, len, ix)
     elif x.nt == ntStream:
       # let len = node.forBody.stmtList.len
       # for i, y in x.streamContent:
@@ -66,5 +68,5 @@ proc handleForStmt(c: Compiler, node, parent: Node, scope: ScopeTable) =
       for item in items(x.streamContent):
         node.forStorage[node.forItem.varName].varValue = newStream item
         for n in node.forBody.stmtList:
-          c.handleInnerNode(n, parent, node.forStorage, len)     
+          c.handleInnerNode(n, parent, node.forStorage, len, ix)     
   else: discard
